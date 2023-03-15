6186db22763a863d814876cc24afb3b1cdd666d3
Josh W <jbweb89@yahoo.com>
2023-03-15 15:58:12 -0400

    copy from p1

---
+++ database.py
@@ -0,0 +1,160 @@
+import sqlite3
+
+conn = sqlite3.connect('stocks.db')
+
+
+def init():
+    conn.execute('''create table if not exists Users
+            (
+                ID INTEGER PRIMARY KEY,
+                first_name varchar(255),
+                last_name varchar(255),
+                user_name varchar(255) NOT NULL,
+                password varchar(255),
+                usd_balance DOUBLE NOT NULL
+                ); ''')
+    conn.execute('''create table if not exists Stocks
+            (
+                ID INTEGER PRIMARY KEY,
+                stock_symbol varchar(4) NOT NULL,
+                stock_balance DOUBLE,
+                user_id int,
+                FOREIGN KEY (user_id) REFERENCES Users(ID)
+                ); ''')
+
+    user = conn.execute('''SELECT * FROM Users WHERE ID = 1;''')
+
+    if (user.fetchone() is None):
+        conn.execute('''INSERT INTO Users (ID, first_name, last_name, user_name, password, usd_balance)
+        VALUES (1, 'John', 'Doe', 'johndoe', 'password', 100);
+        ''')
+
+    conn.commit()
+
+
+def close():
+    conn.close()
+
+
+def getUser(user_id):
+    cursor = conn.execute('''SELECT * FROM Users WHERE ID = ?''', (user_id,))
+    row = cursor.fetchone()
+    if row is None:
+        return None
+    else:
+        return row
+
+
+def buy_stock(stock_symbol, stock_balance, stock_price, user_id):
+    # application allows 1 client currently
+    # checks to make sure passed id=1
+    if (getUser(user_id) == None):
+        return "User"+str(user_id)+" doesn't exist"
+    # Check if user has enough money
+    user = conn.execute('''SELECT * FROM Users
+    WHERE ID = ?;
+    ''', (user_id,)).fetchone()
+
+    if (user[5] < stock_balance * stock_price):
+        return "400 Not enough money"
+
+    # Check if user already has stock
+    stock = conn.execute('''SELECT * FROM Stocks
+    WHERE stock_symbol = ? AND user_id = ?;
+    ''', (stock_symbol, user_id)).fetchone()
+
+    if (stock is not None):
+        # Update stock balance
+        stock = conn.execute('''UPDATE Stocks
+        SET stock_balance = stock_balance + ?
+        WHERE stock_symbol = ? AND user_id = ?;
+        ''', (stock_balance, stock_symbol, user_id))
+    else:
+        # Add stock
+        conn.execute('''INSERT INTO Stocks (stock_symbol, stock_balance, user_id)
+        VALUES (?, ?, ?);
+        ''', (stock_symbol, stock_balance, user_id))
+    # Adjust balance
+    user = conn.execute('''UPDATE Users
+    SET usd_balance = usd_balance - ?
+    WHERE ID = ?;
+    ''', (stock_balance * stock_price, user_id))
+    conn.commit()
+
+    # Fetch user and stock balance
+    user = conn.execute('''SELECT * FROM Users
+    WHERE ID = ?;
+    ''', (user_id,)).fetchone()
+    stock = conn.execute('''SELECT * FROM Stocks
+    WHERE stock_symbol = ? AND user_id = ?;
+    ''', (stock_symbol, user_id)).fetchone()
+
+    return "200 ok \nBOUGHT: New balance: " + str(stock[2]) + " " + stock[1].upper() + " USD balance: " + str(user[5])
+
+
+def sell_stock(stock_symbol, stock_balance, stock_price, user_id):
+    # application allows 1 client currently
+    # checks to make sure passed id=1
+    if (getUser(user_id) == None):
+        return "User"+str(user_id)+" doesn't exist"
+
+    # Check if user has enough stock
+    stock = conn.execute('''SELECT * FROM Stocks
+    WHERE stock_symbol = ? AND user_id = ?;
+    ''', (stock_symbol, user_id)).fetchone()
+
+    if (stock is None or stock[2] < stock_balance):
+        return "400 Not enough stock"
+
+    # Delete stock if balance is 0
+    if (stock[2] == stock_balance):
+        conn.execute('''DELETE FROM Stocks
+        WHERE stock_symbol = ? AND user_id = ?;
+        ''', (stock_symbol, user_id))
+    else:
+        # Update stock balance
+        conn.execute('''UPDATE Stocks
+        SET stock_balance = stock_balance - ?
+        WHERE stock_symbol = ? AND user_id = ?;
+        ''', (stock_balance, stock_symbol, user_id))
+
+    # Adjust balance
+    conn.execute('''UPDATE Users
+    SET usd_balance = usd_balance + ?
+    WHERE ID = ?;
+    ''', (stock_balance * stock_price, user_id))
+    conn.commit()
+
+    # Fetch user and stock balance
+    user = conn.execute('''SELECT * FROM Users
+    WHERE ID = ?;
+    ''', (user_id,)).fetchone()
+    stock = conn.execute('''SELECT * FROM Stocks
+    WHERE stock_symbol = ? AND user_id = ?;
+    ''', (stock_symbol, user_id)).fetchone()
+
+    stockBalance = stock[2] if stock is not None else 0
+
+    return "200 ok \nSOLD: New balance: " + str(stockBalance) + " " + stock_symbol.upper() + " USD balance: " + str(user[5])
+
+
+def list_stocks(user_id):
+    rows = conn.execute('''SELECT * FROM Stocks
+    WHERE user_id = ?;
+    ''', (user_id,)).fetchall()
+
+    if (rows is None or len(rows) == 0):
+        return "200 ok \nNo stocks"
+
+    stocks = ""
+    for row in rows:
+        stocks += row[1] + " " + str(row[2]) + " " + str(row[3]) + "\n"
+
+    return "200 ok \n" + stocks.strip()
+
+
+def get_balance(user_id):
+    user = conn.execute('''SELECT usd_balance, first_name, last_name FROM Users
+    WHERE ID = ?;
+    ''', (user_id,)).fetchone()
+    return "200 ok \nBalance for user " + user[1] + " " + user[2] + ": $" + str(user[0])
---
+++ server.py
@@ -0,0 +1,79 @@
+# Create a socket server that listens on port 8000 and logs messages from clients
+# to the console.
+
+import socket
+
+import database
+
+database.init()
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = ("127.0.0.1", 8000)
+print('starting up on %s' % server_address[0])
+sock.bind(server_address)
+
+# Listen for incoming connections
+sock.listen(1)
+
+
+connection, address = sock.accept()  # accept new connection
+print("Connection from: " + str(address))
+
+
+def process_data(data, connection: socket.socket):
+    print("Received: " + data)
+    data = data.lower().strip().split(" ")
+    if not(data[0].isalpha()):
+        return "400 Invalid command format"
+    if data[0] == "buy":
+        # Check command
+        if (len(data) != 5):
+            return "400 Invalid command format"
+
+        return database.buy_stock(data[1].upper(), float(data[2]), float(data[3]), int(data[4]))
+    elif data[0] == "sell":
+        # Check command
+        if (len(data) != 5):
+            return "400 Invalid command format"
+
+        return database.sell_stock(data[1].upper(), float(data[2]), float(data[3]), int(data[4]))
+    elif data[0] == "list":
+        return database.list_stocks(1)
+    elif data[0] == "balance":
+        return database.get_balance(1)
+    elif data[0] == "quit":
+        connection.send("200 OK".encode())
+        connection.close()
+        database.close()
+        return None
+    elif data[0] == "shutdown":
+        connection.send("200 OK".encode())
+        connection.close()
+        sock.close()
+        database.close()
+        exit()
+    else:
+        return "400 Invalid command"
+
+
+while True:
+
+    # receive data stream. it won't accept data packet greater than 1024 bytes
+    data = connection.recv(1024).decode()
+    if not data:
+        # if data is not received break
+        break
+
+    message = process_data(data, connection)
+
+    if (message is None):
+        connection, address = sock.accept()  # accept new connection
+        continue
+
+    # send data to the client
+    connection.send(str(message).encode())
+
+connection.close()
